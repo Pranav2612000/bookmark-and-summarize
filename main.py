c63d2b0a75245d1be2ad1addaf4ac5379971c6bc
vishal-pathare <vishal.1461999@gmail.com>
2020-11-08 22:32:20 +0530

    flask changes

--- main.py
+++ main.py
@@ -7,12 +7,30 @@ from utils import dynamodb_utils, get_summary
 
 app = Flask(__name__, template_folder = 'html_src/')
 
-#Home Page which is currently for signup
-# @app.route('/', methods = ['GET', 'POST'])
-# def default():
-    #The logging in page to be rendered 
+@app.route('/', methods = ['GET', 'POST'])
+def sign_in():
+    if request.method == 'POST':
+        #Get the details of the user
+        user_id = request.form['UserID']
+        password = request.form['Password']
 
-    # return render_template('login.html')
+        
+        ret_val = dynamodb_utils.verify_login(user_id, password)
+
+        #User does not exist
+        if ret_val == 0:
+            return render_template('user_not_exist.html')
+
+        #password is wrong
+        elif ret_val == 1:
+            return render_template('password_wrong.html')
+
+        #Login successful, go to home page
+        else:
+            return redirect("/Home")
+
+    else:
+        return render_template('login.html')
 
 #Processing the sign up request
 @app.route('/SignUp', methods = ['GET', 'POST'])
@@ -50,36 +68,23 @@ def new_user():
     except:
         return "Internal Server Error"
 
-@app.route('/', methods = ['GET', 'POST'])
-def sign_in():
-    if request.method == 'POST':
-        #Get the details of the user
-        user_id = request.form['UserID']
-        password = request.form['Password']
 
-        
-        ret_val = dynamodb_utils.verify_login(user_id, password)
 
-        #User does not exist
-        if ret_val == 0:
-            return render_template('user_not_exist.html')
-
-        #password is wrong
-        elif ret_val == 1:
-            return render_template('password_wrong.html')
-
-        #Login successful, go to home page
-        else:
-            return redirect("/Home")
-
-    else:
-        return render_template('login.html')
 
 @app.route('/Home', methods = ['GET', 'POST'])
 def render_home():
+
+    if request.method == 'POST':
+
+        user_id = ...
+
+        url = request.form['URL']
+
+        dynamodb_utils.add_bookmark(user_id, url)
+
     return render_template('home.html')
 
-@app.route('/Add_bookmark', methods = ['GET', 'POST'])
+'''@app.route('/Add_bookmark', methods = ['GET', 'POST'])
 def add_bookmark():
     try:
         if request.method == 'POST':
@@ -96,7 +101,7 @@ def add_bookmark():
             return redirect('/Home')
 
     except:
-        return 'Internal Server error'
+        return 'Internal Server error'''
 
 @app.route('/Bookmarks', methods = ['GET', 'POST'])
 def display_bookmarks():
