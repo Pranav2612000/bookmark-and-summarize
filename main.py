6ba139f85babc5ba4a135b6a0ce1af08a7f25823
Pranav <pranav2000joglekar@gmail.com>
2020-11-08 12:25:11 +0530

    feat: return text response, add try-except blocks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 import sys
 import os
 from utils import *
+from utils import add_user_to_db
 #sys.path.append("..")
 
 
@@ -15,19 +16,24 @@ def default():
 #Processing the sign up request
 @app.route('/SignUp', methods = ['GET', 'POST'])
 def new_user():
-    if request.method == 'POST':
-        #Get the details of the user
-        user_id = request.form['UserID']
-        password = request.form['Password']
+    try:
+        if request.method == 'POST':
+            #Get the details of the user
+            user_id = request.form['UserID']
+            password = request.form['Password']
 
-        print ("User ID and password : {} and {}".format(user_id, password))
-        ret_val = add_user_to_db.add_user_to_db(user_id, password)
+            print ("User ID and password : {} and {}".format(user_id, password))
+            ret_val = add_user_to_db.add_user_to_db(user_id, password)
 
-        if ret_val == True:
-            return render_template('sign_up.html')
+            if ret_val == True:
+                #return render_template('sign_up.html')
+                return "User Added Successfully"
 
-        else:
-            return render_template('failed.html')
+            else:
+                #return render_template('failed.html')
+                return "Failed to add user"
+    except:
+        return "Internal Server Error"
 
 
 if __name__ == "__main__":
